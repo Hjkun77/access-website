bfd22a36e4641ffd720f08f05a339a39174b25e0
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-10-30 14:41:14 +0800

    Added new FAQs

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,33 @@ if (t < 0) {
         document.getElementById("seconds").innerHTML = "00";
     }
 }, 1000);
+
+
+$('.carousel').carousel({
+  interval: 5000,
+});
+
+$('.dot-1').click(function () {
+  $('.carousel').carousel(0)
+})
+$('.dot-2').click(function () {
+  $('.carousel').carousel(1)
+})
+$('.dot-3').click(function () {
+  $('.carousel').carousel(2)
+})
+$('.dot-4').click(function () {
+  $('.carousel').carousel(3)
+})
+$('.dot-5').click(function () {
+  $('.carousel').carousel(4)
+})
+$('.dot-6').click(function () {
+  $('.carousel').carousel(5)
+})
+$('.dot-7').click(function () {
+  $('.carousel').carousel(6)
+})
+$('.dot-8').click(function () {
+  $('.carousel').carousel(7)
+})
