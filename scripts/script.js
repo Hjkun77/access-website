e55b28f8d57f22cf8391948df4581e5b308f4fbe
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2018-11-01 18:19:10 +0800

    spreadsheet posting

--- scripts/script.js
+++ scripts/script.js
@@ -71,3 +71,40 @@ $('.dot-7').click(function () {
 $('.dot-8').click(function () {
   $('.carousel').carousel(7)
 })
+
+// google sheet
+// https://docs.google.com/spreadsheets/d/1fNq4jiIlJeEhN3NsnSAM8CEPUzEpxGHHOyl1IJpHHUo/edit#gid=0
+const sheetURL = 'https://script.google.com/macros/s/AKfycbx_JApnE0GiZ3cWIkQWHOsrmg0oeeeJ-ImHZ2xxUGe-QdFje1bT/exec'
+
+const form = $('.form')
+
+$.fn.serializeObject = function()
+{
+ var o = {};
+ var a = this.serializeArray();
+ $.each(a, function() {
+ if (o[this.name]) {
+ if (!o[this.name].push) {
+ o[this.name] = [o[this.name]];
+ }
+ o[this.name].push(this.value || '');
+ } else {
+ o[this.name] = this.value ||'';
+ }
+ });
+ return o;
+};
+
+
+$('.contact-button').on('click', function(e) {
+  e.preventDefault()
+  $.ajax({
+    url: sheetURL,
+    method: 'GET',
+    dataType: 'json',
+    data:  form.serializeObject(),
+    success: function() {
+      form.trigger('reset')
+    }
+  })
+})
