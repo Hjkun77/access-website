3dd1a9d96b9e025d7e6f3e485423d290706cee26
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-11-01 18:36:24 +0800

    Backend

---
+++ scripts/carousel.js
@@ -0,0 +1,28 @@
+$('.carousel').carousel({
+  interval: 5000,
+});
+
+$('.dot-1').click(function () {
+  $('.carousel').carousel(0)
+})
+$('.dot-2').click(function () {
+  $('.carousel').carousel(1)
+})
+$('.dot-3').click(function () {
+  $('.carousel').carousel(2)
+})
+$('.dot-4').click(function () {
+  $('.carousel').carousel(3)
+})
+$('.dot-5').click(function () {
+  $('.carousel').carousel(4)
+})
+$('.dot-6').click(function () {
+  $('.carousel').carousel(5)
+})
+$('.dot-7').click(function () {
+  $('.carousel').carousel(6)
+})
+$('.dot-8').click(function () {
+  $('.carousel').carousel(7)
+})
